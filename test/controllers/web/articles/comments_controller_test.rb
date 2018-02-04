48a21637ae820ad2385b2d382493aa061a1a1d8c
vtm <vtmilyakov@yandex.ru>
2018-02-04 09:49:51 +0300

    Fix tests

--- test/controllers/web/articles/comments_controller_test.rb
+++ test/controllers/web/articles/comments_controller_test.rb
@@ -3,15 +3,17 @@ require 'test_helper'
 class Web::Articles::CommentsControllerTest < ActionDispatch::IntegrationTest
   test 'should create comment' do
     article = articles(:published)
-    params = { commentor: 'me', body: 'some text' }
+    params = { commentor: 'test', body: 'new_comment' }
     post article_comments_url(article.id), params: { article_comment: params }, headers: auth_headers
-    assert_response :redirect
+
+    comment = article.comments.find_by(params)
+    assert {comment.persisted?}
   end
 
   test 'should destroy comment' do
     comment = article_comments(:one)
-    params = { commentor: 'me', body: 'some text' }
-    post article_comments_url(comment.article.id), params: { article_comment: params }, headers: auth_headers
-    assert_response :redirect
+    delete article_comment_url(comment.article.id, comment.id), headers: auth_headers
+
+    assert { !Article::Comment.exists? id: comment }
   end
 end
